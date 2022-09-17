b19e3b5504cd433a9e64cd7b7836de60f814b194
BeejayBolaj <bravo2sailor@gmail.com>
2022-09-17 19:27:33 +0000

    2-print_alphabet_x10.c

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -14,10 +14,8 @@ void print_alphabet_x10(void)
 	{
 		for (j = 'a'; j <= 'z'; j++)
 		{
-			for (j = 'a'; j <= 'z'; j++)
-			{
-				_putchar(j);
-			}
-			_putchar('\n');
+			_putchar(j);
+		}
+		_putchar('\n');
 	}
 }
